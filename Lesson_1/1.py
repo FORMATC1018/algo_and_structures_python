0adb71e6925cf4eceb8b2c4e8fe0596e5027485c
FORMATC1018 <kraskokirill1018@mail.ru>
2019-03-10 17:19:11 +0300

    Удалил ненужные файлы

--- Lesson_1/1.py
+++
@@ -1,6 +0,0 @@
-# 1.	Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
-number = int(input("Введите трехзначное число "))
-sum_digit = number%10 + number//100 + (number//10%10)
-mult_digit = number%10 * number//100 * (number//10%10)
-print ("Сумма цифр равна {}".format(sum_digit))
-print ("Произведение цифр равно {}".format(mult_digit))
