d905b78bf772438e0b2b9cfd9d735421c47abbad
FORMATC1018 <kraskokirill1018@mail.ru>
2019-03-11 21:03:08 +0300

    Доделал дз урок2

--- Lesson_2/8.py
+++ Lesson_2/8.py
@@ -3,3 +3,23 @@
  последовательности чисел. Количество вводимых чисел и цифра,
  которую необходимо посчитать, задаются вводом с клавиатуры.
 """
+
+def find_digit(number, digit):
+    count=0
+    while number!=0:
+        last_digit = number%10
+        if last_digit==digit:
+            count+=1
+        number//=10
+    return count
+
+
+n = int(input("Введите количество чисел - "))
+digit = int(input("Введите искомую цифру - "))
+count=0
+count_digit=0
+for i in range(n):
+    number = int(input("Введите число - "))
+    count_digit = find_digit(number,digit)
+    count = count + count_digit
+print("Цифра {} встречается {} раз".format(digit, count))
--- Lesson_2/9.py
+++ Lesson_2/9.py
@@ -2,3 +2,21 @@
 9. Среди натуральных чисел, которые были введены, найти
 наибольшее по сумме цифр. Вывести на экран это число и сумму его цифр.
 """
+def sum_digit(number):
+    sum=0
+    while number!=0:
+        last_digit = number%10
+        sum+=last_digit
+        number//=10
+    return sum
+
+
+n = int(input("Введите количество чисел - "))
+maximum=0
+for i in range(n):
+    number = int(input("Введите число - "))
+    sum = sum_digit(number)
+    if sum>maximum:
+        maximum=sum
+        find_numb = number
+print("Наибольшее по сумме цифр число - {} Сумма цифр этого числа {} ".format(find_numb, maximum))
